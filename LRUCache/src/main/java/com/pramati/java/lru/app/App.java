02c6cb4f83e6e911f533aca0216e74e61e4990cb
virendrac <35448511+virendrac@users.noreply.github.com>
2018-02-07 15:09:54 +0530

    LRU Cache. removed sysout

--- LRUCache/src/main/java/com/pramati/java/lru/app/App.java
+++ LRUCache/src/main/java/com/pramati/java/lru/app/App.java
@@ -7,20 +7,18 @@ public class App {
 
     public static void main(String [] args){
 
-        int cacheSize=Integer.parseInt(PropertiesGetter.getProperty("lruCacheSize"));
         LRUCache<Character> cache=new LRUCache<Character>(3);
 
         cache.add(new Character('A'));
         cache.add(new Character('B'));
         cache.add(new Character('C'));
-        System.out.println("ABC 1");
+
         cache.printCache();
 
         cache.add(new Character('D'));
-        System.out.println("BCD 2");
+
         cache.printCache();
         cache.add(new Character('C'));
-        System.out.println("BDC 3");
         cache.printCache();
     }
 }
